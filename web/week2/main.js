4bf036ba5ce44079f507d685667cfab70fdbccde
Stephen Nielson <stephen@nielson.org>
2018-09-22 13:24:10 -0400

    Finished Stretch goals for team activity

--- web/week2/main.js
+++ web/week2/main.js
@@ -1,13 +1,24 @@
-document.getElementById('action').addEventListener('click', function() {
-	alert('Clicked!');
-});
+$(document).ready(function() {
 
-document.getElementById('actionChangeColor').addEventListener('click', function() {
-	var color = document.getElementById('color').value || "";
-	if (color.trim() == "") {
-		alert("Enter in a color to change");
-		return;
-	}
-	var div1 = document.getElementById('div1');
-	div1.style['background-color'] = color;
+	/** The amount of time in miliseconds that the fade animation should take **/
+	var fadeTimeDelay = 1500;
+
+	$("#action").click(function() {
+		alert('Clicked!');
+	});
+	$("#actionChangeColor").click(function() {
+		var color = $("#color").val() || "";
+		if (color.trim() == "") {
+			alert("Enter in a color to change");
+			return;
+		}
+		
+		$("#div1").css('background-color', color);
+	});
+	$("#actionFade").click(function() {
+		$("#div3").fadeOut(fadeTimeDelay);
+	});
+	$("#actionDisplay").click(function() {
+		$("#div3").fadeIn(fadeTimeDelay);
+	});
 });
